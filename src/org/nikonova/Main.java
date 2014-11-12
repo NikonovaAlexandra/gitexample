16175fccdce1d48f0c2a245b081452f4a2898797
Alexandra Nikonova <a.nikonova@sam-solutions.com>
2014-11-12 07:27:17 -0300

    use service

--- src/org/nikonova/Main.java
+++ src/org/nikonova/Main.java
@@ -2,8 +2,8 @@ package org.nikonova;
 
 public class Main {
  public static void main (String [] args) {
-	 System.out.print("Hello ");
-	 System.out.println("World!");
+	 MyService service = new MyService();
+	 service.print("Hello World!");
 	 
  }
 }
